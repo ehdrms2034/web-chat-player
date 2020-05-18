fa4d46283007801277919505dfc1e7e468b09383
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2020-05-18 18:13:28 +0900

    소켓 메시지
    - join 메시지(on) : 영상을 보러 들어왔을때 같은 소켓 room에 들어오도록 포함
    - newComment 메시지(on) : 새로운 댓글을 입력했을 때 그걸 받아서 다시 같은 비디오를 보는 모든 유저에게 소켓메시지 보냄
    - newMessage 메시지(emit) : 같은 비디오를 보는 모든 유저에게 새로운 메시지에 대한 정보 보냄

--- backend/comment-and-db/controller/socket/socket.js
+++ backend/comment-and-db/controller/socket/socket.js
@@ -6,6 +6,18 @@ module.exports = (server) => {
     io.on('connect', (socket) => {
         console.log('socket.io connected!');
 
+        socket.on('join', (video) => {
+            console.log('socket join!');
+            
+            socket.join(video);
+        });
+
+        socket.on('newComment', (id, message, createdAt, timeline, video) => {
+            console.log('socket newComment!');
+
+            io.to(video).emit('newMessage', { id: id, message: message, createdAt: createdAt, timeline: timeline });
+        });
+
         socket.on('disconnect', () => {
             console.log('socket.io disconnected!');
         });
