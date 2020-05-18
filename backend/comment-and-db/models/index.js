80382cf471456f838ceb7e8c65d4476a8db5571c
KimDongGeun <ehdrms2034@naver.com>
2020-05-18 17:40:34 +0900

    DB Connection Error handling 수정

--- backend/comment-and-db/models/index.js
+++ backend/comment-and-db/models/index.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 const { DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 const url = `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
 
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-});
+try {
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+} catch (error) {
+    console.log("mongoDB Connection Error : ",error);
+}
 
 const db = mongoose.connection;
 db.url = url;
@@ -15,8 +19,8 @@ db.on("open", () => {
   console.log("DB Connection on");
 });
 
-db.on("error", () => {
-  console.log("connection Error");
+db.on("error", error => {
+  console.log("MongoDB Error : ", error);
 });
 
 module.exports = db;
