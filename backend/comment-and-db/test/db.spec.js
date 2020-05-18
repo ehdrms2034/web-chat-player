af5f7e031fbb089390a81b22ee0facecea7ea8a1
KimDongGeun <ehdrms2034@naver.com>
2020-05-18 17:13:35 +0900

    nodemon test case 변경 npm 스크립팅 추가

--- backend/comment-and-db/test/db.spec.js
+++ backend/comment-and-db/test/db.spec.js
@@ -1,7 +1,20 @@
+var path = require("path");
+const dotenv = require("dotenv").config({
+    path: path.join(__dirname, "..//config/.env"),
+  });
+
+const mongoose = require("../models");
+const UserService = require('../services/db/UserService');
 
 
-describe("test",()=>{
-    it("test",()=>{
-        console.log("hi");
-    });
-})
+describe("db", () => {
+  beforeEach(() => {
+    mongoose;
+  });
+
+  it("test", async () => {
+    const userService = new UserService();
+    await userService.createUser("cookie","ooo");
+    console.log(process.env.DB_HOST);
+  });
+});
