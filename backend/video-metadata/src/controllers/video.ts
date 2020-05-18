e4edf2810217242c3c61f430368163947d0db593
RoenissMoon <roeniss2@gmail.com>
2020-05-18 16:39:57 +0900

    API 인터페이스 및 라우팅 설정 (테스트를 위한 더미 데이터)

--- backend/video-metadata/src/controllers/video.ts
+++ backend/video-metadata/src/controllers/video.ts
@@ -1,5 +1,6 @@
 import fetch, { RequestInit } from "node-fetch";
 import { Video, VideoWrapper } from "../types/video";
+import dummy from "./dummyData";
 
 export async function getVideoList(): Promise<Video[]> {
   const URL = `//${process.env.DB_SERVER}/videos`;
@@ -8,9 +9,12 @@ export async function getVideoList(): Promise<Video[]> {
     method: "GET",
   };
   try {
+    /* TODO: dummy data --> real API call
     const res = await fetch(URL, options);
     const resData = <VideoWrapper>await res.json();
-    return resData["data"];
+    return resData["data"]; */
+
+    return dummy;
   } catch (error) {
     throw error;
   }
