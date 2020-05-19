e41f865782e64dd00027f3e10b7cc293e46d5729
KimDongGeun <ehdrms2034@naver.com>
2020-05-19 13:52:24 +0900

    add cors orgin allow

--- backend/comment-and-db/app.js
+++ backend/comment-and-db/app.js
@@ -7,6 +7,7 @@ const dotenv = require("dotenv").config({
   path: path.join(__dirname, "/config/.env"),
 });
 const mongooseDB = require("./models");
+const cors = require('cors');
 
 const router = require("./controller/index");
 
@@ -16,6 +17,7 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
