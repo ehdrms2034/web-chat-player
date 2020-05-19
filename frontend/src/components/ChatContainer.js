f8fa783e92bdd8b3f525ca02873eb814e4ffefe2
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2020-05-19 18:11:44 +0900

    몇가지 오류 수정
    - ENDPOINT 수정 (server4로 하고있었음 -> server3으로)
    - newComment 보내는 부분 Object 값 다이렉트로 넣음

--- frontend/src/components/ChatContainer.js
+++ frontend/src/components/ChatContainer.js
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 import Comments from './Comments';
 import Input from './Input';
 
-let socket, video;
+let socket;
 
 const ChatContainer = ({ _name, _timeline }) => {
   const [name, setName] = useState('');
@@ -12,7 +12,7 @@ const ChatContainer = ({ _name, _timeline }) => {
   const [timeline, setTimeline] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'ws://49.50.162.195:3000'
+  const ENDPOINT = 'ws://49.50.173.151:3000'
 
   useEffect(() => {
     const name = _name;
@@ -20,7 +20,7 @@ const ChatContainer = ({ _name, _timeline }) => {
 
     socket = io(ENDPOINT);
 
-    video = 'video1';   // 임시데이터. 나중에 영상별 구분할 수 있는 고유값을 보내줘야 함.
+    let video = 'video1';   // 임시데이터. 나중에 영상별 구분할 수 있는 고유값을 보내줘야 함.
     socket.emit('join', video);
     console.log('send join message');
   }, [ENDPOINT, _name]);
@@ -45,9 +45,14 @@ const ChatContainer = ({ _name, _timeline }) => {
 
     if(message) {
       // 원래 이건데 임시 데이터로 대체함.
-      // socket.emit('newComment', ({name, message, createdAt, timeline}), () => setMessage(''));
-      const n = 'name', c = '11:22:33', t = '33:22:11';
-      socket.emit('newComment', ({n, message, c, t, video}), () => setMessage(''));
+      // socket.emit('newComment', ({name, message, createdAt, timeline}), () => setMessage(''))
+      socket.emit('newComment', ({
+        id: '닉네임', 
+        message: message, 
+        createdAt: '생성시간', 
+        timeline: '타임라인', 
+        video: 'video1'
+      }), () => setMessage(''));
       // DB REST API 들어가야함.
     }
   }
