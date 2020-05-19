9a6e6dc6ed04e627a6a4705f7825ea3c51b5188f
Um seoyoung <tjdud0123@naver.com>
2020-05-19 18:30:02 +0900

    헤더컬러 수정,변수이름 수정

--- frontend/src/components/MovieCard.js
+++ frontend/src/components/MovieCard.js
@@ -7,7 +7,7 @@ function MovieCard(props) {
   const video = props.video;
   const date = video.uploadedAt.slice(0, 10).split("-").join(".");
   const url = `videos/${video._id}`;
-  const slicedSum = video.summary.length > 60 ? `${video.summary.slice(0,60)}...` : video.summary
+  const slicedSummary = video.summary.length > 60 ? `${video.summary.slice(0,60)}...` : video.summary
 
   return (
     <Link to={url}>
@@ -17,7 +17,7 @@ function MovieCard(props) {
         </div>
         <div className="MovieInfo">
           <h3 className="MovieTitle"> {video.name} </h3>
-          <div className="MovieSummary"> {slicedSum} </div>
+          <div className="MovieSummary"> {slicedSummary} </div>
           <div className="MovieDate"> {date} </div>
         </div>
       </div>
