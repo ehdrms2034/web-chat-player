11ac30ebafbe38c5b2c88a7a9e26f540865179e8
Um seoyoung <tjdud0123@naver.com>
2020-05-19 09:49:24 +0900

    레이아웃 css 입히기 전

--- frontend/src/components/PlayView.js
+++ frontend/src/components/PlayView.js
@@ -1,15 +1,32 @@
 import React from "react";
 import "../css/playView.css";
+import Screen from "./Screen.js";
+import ChatContainer from "./ChatContainer.js";
 
 function PlayView({ match }) {
+  // 파람스에서 아이디 말고 영상 정보를 다 받아왔으면 하는데 방법 찾는 중
   const movieId = match.params;
+  console.log(match.params);
   console.log(movieId);
 
+  // 아이디로 영상정보 받아오기 **
   if (!movieId) {
     return <div> 존재하지 않는 영상입니다 </div>;
   }
 
-  return <div className="PlayView"> 플레이 뷰! </div>;
+  return (
+    <div className="PlayView">
+      <section className="video">
+        <Screen />
+        <div className="playInfo">
+          <div className="MovieTitle">영화 제목</div>
+          <div className="MovieSummary">내용</div>
+          <div className="MovieDate">날짜</div>
+        </div>
+      </section>
+      <ChatContainer />
+    </div>
+  );
 }
 
 export default PlayView;
---
+++ frontend/src/components/Screen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// 플레이어 사용 **
+
+function Screen() {
+  return <div className="Screen">스크린</div>;
+}
+
+export default Screen;
