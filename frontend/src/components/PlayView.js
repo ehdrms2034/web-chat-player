ead18e72dcfd05e7d495c0d467ea3ed64d87d8f5
RoenissMoon <roeniss2@gmail.com>
2020-05-19 11:08:24 +0900

    목업 데이터 -> axios를 이용한 비디오 메타데이터 호출

--- frontend/src/components/PlayView.js
+++ frontend/src/components/PlayView.js
@@ -2,14 +2,19 @@ import React from "react";
 import "../css/playView.css";
 import Screen from "./Screen.js";
 import ChatContainer from "./ChatContainer.js";
+import axios from "axios";
 
 function PlayView({ match }) {
-  // 파람스에서 아이디 말고 영상 정보를 다 받아왔으면 하는데 방법 찾는 중
-  const movieId = match.params;
-  console.log(match.params);
-  console.log(movieId);
+  // 파람스에서 아이디 말고 영상 정보를 다 받아왔으면 하는데 방법 찾는 중 --> 찾으시기 전까진 아래 방법으로 가시죠
+  const movieId = match.params.id;
+  const [video, setVideos] = React.useState({});
+  React.useEffect(() => {
+    axios
+      .get(`http://27.96.135.160:3000/video/${movieId}`)
+      .then((res) => setVideos(res.data))
+      .catch((err) => setVideos([]));
+  }, [movieId]);
 
-  // 아이디로 영상정보 받아오기 **
   if (!movieId) {
     return <div> 존재하지 않는 영상입니다 </div>;
   }
@@ -19,9 +24,11 @@ function PlayView({ match }) {
       <section className="video">
         <Screen />
         <div className="playInfo">
-          <div className="MovieTitle">영화 제목</div>
-          <div className="MovieSummary">내용</div>
-          <div className="MovieDate">날짜</div>
+          <div className="MovieTitle">{video.name}</div>
+          <div className="MovieSummary">{video.summary}</div>
+          <div className="MovieDate">
+            {new Date(video.uploadedAt).toLocaleDateString()}
+          </div>
         </div>
       </section>
       <ChatContainer />
