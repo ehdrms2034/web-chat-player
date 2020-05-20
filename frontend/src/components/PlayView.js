c8cf4eb0d95bc9d42f78bf83de74393b5a3bb584
Sieun Sim <tlatldms@naver.com>
2020-05-20 15:53:20 +0900

    added react player func

--- frontend/src/components/PlayView.js
+++ frontend/src/components/PlayView.js
@@ -8,7 +8,8 @@ function PlayView({ match }) {
   const videoId = match.params.id;
   const [video, setVideos] = React.useState({});
   const METADATA_BASE_URL = "http://27.96.130.172/api/video";
-
+  const [videoTime, handleTime] = React.useState(0);
+  let _player = "";
   React.useEffect(() => {
     axios
       .get(`${METADATA_BASE_URL}/video/${videoId}`)
@@ -16,22 +17,37 @@ function PlayView({ match }) {
       .catch((err) => setVideos([]));
   }, [videoId]);
 
+  const onPlayerReady = () => {
+    const hlsInstance = _player.getInternalPlayer("hls");
+    //console.log(hlsInstance);
+    // Note that we can assume we have the `Hls` global here
+    // as the player should be ready at this point
+    hlsInstance.on(window.Hls.Events.FRAG_LOADED, (data) => {
+      //console.log("FRAG_LOADED", data);
+    });
+  };
+
   if (!videoId) {
     return <div> 존재하지 않는 영상입니다 </div>;
   }
-
+  const ref = (player) => {
+    _player = player;
+  };
+  const onProgress = (p) => {
+    handleTime(p.playedSeconds);
+  };
   return (
     <div className="PlayView">
       <section className="video">
         <ReactPlayer
           className="Screen"
-          // ref={ref}
+          ref={ref}
           url={"http://49.50.162.195:8080/videos/whatsuda.m3u8"}
           playing
-          // onReady={onPlayerReady}
+          onReady={onPlayerReady}
           controls={true}
           onSeek={(e) => console.log(e)}
-          // onProgress={onProgress}
+          onProgress={onProgress}
         />
         <div className="playInfo">
           <div className="MovieTitle">{video.name}</div>
