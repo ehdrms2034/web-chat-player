770b1fbe0348a5a577c47f8c501b4bab6c3b5f28
KimDongGeun <ehdrms2034@naver.com>
2020-05-22 00:05:46 +0900

    addupload-routing

--- backend/video-metadata/src/routes/index.ts
+++ backend/video-metadata/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllVideos, getSingleVideo } from "../controllers/video";
+import { getAllVideos, getSingleVideo, saveSigleVideo } from "../controllers/video";
 
 const router = express.Router();
 
@@ -19,6 +19,18 @@ router.get("/videos", async (_req, res) => {
     return res.json(data);
   } catch (error) {
     return res.sendStatus(404);
+  }
+});
+
+router.post("/video", async (req, res) => {
+  const { name, summary, posterUrl, videoUrl } = req.body;
+  try {
+    if (name === undefined || summary === undefined || posterUrl === undefined || videoUrl === undefined)
+      throw new Error("request body값이 없음");
+    await saveSigleVideo(name, summary, posterUrl, videoUrl);
+    return res.json({ response: "success", message: "성공적으로 영화를 DB에 저장했습니다." });
+  } catch (error) {
+    return res.json({ response: "error", message: "DB에 저장하는데 실패했습니다.", data: error.message });
   }
 });
 
